cb9120fc132ccaf69e6d52665c04a3a6c24ee1c5
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-11-08 09:18:45 +0330

    Move port scan to function

--- port_scan.py
+++ port_scan.py
@@ -3,33 +3,38 @@ import sys
 import socket
 
 
-# Get args or show prompt to get host
-if len(sys.argv) > 1:
-    host = sys.argv[1]
-else:
-    host = input("port_scan > ")
+def port_scan(host):
+    try:
+        # Resolve host to ip
+        ip = socket.gethostbyname(host)
+        print(f"Scanning {host} ({ip})")
 
-try:
-    # Resolve host to ip
-    ip = socket.gethostbyname(host)
-    print(f"Scanning {host} ({ip})")
+        # Find open ports of host
+        nm = nmap.PortScanner()
 
-    # Find open ports of host
-    nm = nmap.PortScanner()
+        # Add -sU makes it scan UDP ports and -sS makes it scan TCP ports
+        # Also, -Pn disables host discovery to make scanning faster
+        # -O enables OS fingerprint scan
+        nm.scan(hosts=ip, arguments='-n -sU -sS -Pn -p 1-1024')
 
-    # Add -sU makes it scan UDP ports and -sS makes it scan TCP ports
-    # Also, -Pn disables host discovery to make scanning faster
-    # -O enables OS fingerprint scan
-    nm.scan(hosts=ip, arguments='-n -sU -sS -Pn -p 1-1024')
+        # Print open ports
+        for i in nm[ip]['tcp']:
+            print(f"Port {i} -> {nm[ip]['tcp'][i]['state']}/tcp")
+        for i in nm[ip]['udp']:
+            print(f"Port {i} -> {nm[ip]['udp'][i]['state']}/udp")
 
-    # Print open ports
-    for i in nm[ip]['tcp']:
-        print(f"Port {i} -> {nm[ip]['tcp'][i]['state']}/tcp")
-    for i in nm[ip]['udp']:
-        print(f"Port {i} -> {nm[ip]['udp'][i]['state']}/udp")
+    except socket.gaierror:
+        print("Port scan: Unknown host")
+    except KeyboardInterrupt:
+        print("Port scan: Interrupted")
+        sys.exit(0)
 
-except socket.gaierror:
-    print("Port scan: Unknown host")
-except KeyboardInterrupt:
-    print("Port scan: Interrupted")
-    sys.exit(0)
+
+if __name__ == '__main__':
+    # Get args or show prompt to get host
+    if len(sys.argv) > 1:
+        host = sys.argv[1]
+    else:
+        host = input("port_scan > ")
+
+    port_scan(host)

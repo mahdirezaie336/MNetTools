3c459a533001d5848e04da9f03d73e5df9d96219
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-11-08 09:16:56 +0330

    Add function to ping

--- ping.py
+++ ping.py
@@ -4,30 +4,35 @@ import socket
 import time
 
 
-# Get args or show prompt
-if len(sys.argv) > 1:
-    host = sys.argv[1]
-else:
-    host = input("ping > ")
+def ping(host):
+    # Check if host is valid
+    try:
+        ip = socket.gethostbyname(host)
+        print(f"Pinging {host} ({ip})")
+        nm = nmap.PortScanner()
 
-# Check if host is valid
-try:
-    ip = socket.gethostbyname(host)
-    print(f"Pinging {host} ({ip})")
-    nm = nmap.PortScanner()
+        # Ping host 4 times
+        for i in range(4):
+            # The switch -sn disables port scan and is used for ping hosts
+            nm.scan(hosts=ip, arguments='-sn')
+            hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
 
-    # Ping host 4 times
-    for i in range(4):
-        # The switch -sn disables port scan and is used for ping hosts
-        nm.scan(hosts=ip, arguments='-sn')
-        hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+            for host, status in hosts_list:
+                print(f'Reply from {host} time={nm.scanstats()["elapsed"]}s')
+            time.sleep(1)
 
-        for host, status in hosts_list:
-            print(f'Reply from {host} time={nm.scanstats()["elapsed"]}s')
-        time.sleep(1)
+    except socket.gaierror:
+        print("Ping: Unknown host")
+    except KeyboardInterrupt:
+        print("Ping: Interrupted")
+        sys.exit(0)
 
-except socket.gaierror:
-    print("Ping: Unknown host")
-except KeyboardInterrupt:
-    print("Ping: Interrupted")
-    sys.exit(0)
+
+if __name__ == '__main__':
+    # Get args or show prompt
+    if len(sys.argv) > 1:
+        host = sys.argv[1]
+    else:
+        host = input("ping > ")
+
+    ping(host)
